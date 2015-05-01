51ad7812d356be0bf7517d06f1361e4eebf087bf
Brian Wilson <brianwilson1001@hotmail.com>
2015-05-01 20:01:29 +0100

    Update static address in initial js Update wsgi Update requirements

--- graphs/wsgi.py
+++ graphs/wsgi.py
@@ -1,14 +1,39 @@
 """
-WSGI config for graphs project.
+WSGI config for thatforum project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphs.settings")
+import os, sys, site
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphs.settings")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+_application = django.core.handlers.wsgi.WSGIHandler()
+
+def application(environ, start_response):
+    os.environ['GRAPHS_DB_PASS'] = environ['GRAPHS_DB_PASS']
+    os.environ['DJANGO_SETTINGS_MODULE'] = environ['DJANGO_SETTINGS_MODULE']
+    # if 'DJANGO_CONFIGURATION' in environ:
+    #     import newrelic.agent
+    #     newrelic.agent.initialize('%s/newrelic/newrelic.ini' % Path(__file__).ancestor(2), environ['DJANGO_CONFIGURATION'])
+
+    try:
+        return _application(environ, start_response)
+    except ImportError:
+        # Remember the original sys.path
+        prev_sys_path = sys.path
+        # Add the project folder to path
+        mp = Path(__file__).ancestor(2)
+        site.addsitedir(mp)
+        # Reorder sys.path so new directory is at front
+        new_sys_path = []
+        for item in list(sys.path):
+            if item not in prev_sys_path:
+                new_sys_path.append(item)
+                sys.path.remove(item)
+        sys.path[:0] = new_sys_path
+        return _application(environ, start_response)

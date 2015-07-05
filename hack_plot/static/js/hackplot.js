1a9f67e3cea957629d59abecc95b007efcf74b48
Brian Wilson <brianwilson1001@hotmail.com>
2015-07-05 18:56:09 +0100

    Bug fix: fix paper.safari no a function

--- hack_plot/static/js/hackplot.js
+++ hack_plot/static/js/hackplot.js
@@ -1,10 +1,27 @@
 $(document).ready(function(){
-    $.getJSON('{% url "hackplot_api:list" %}', function(data){
-        console.log('got data');
+    $.getJSON($('#hackplot_chart').attr('data-url'), function(data){
+        geoPlots = {};
+        $.each(data.results, function(index, result){
+            newPlot = {
+                type: 'circle',
+                size: 5,
+                latitude: result.latitude,
+                longitude: result.longitude,
+                attrs: {
+                    opacity: 1
+                },
+                tooltip: {
+                    content: '<span style="font-weight: bold;">' + result.ip_address + '</span><br/>' + result.attempts.length
+                }
+            }
+            geoPlots[result.ip_address] = newPlot
+        });
+
         $('#hackplot_chart').mapael({
             map : {
                 name : "world_countries"
-            }
+            },
+            plots: geoPlots
         });
     });
 });
